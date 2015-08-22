8800c488b724a00a120f98fa11287fd5ec737219
Su Hao <1156407698@qq.com>
2015-08-22 16:39:11 +0800

    .

--- src/com/mcgoodtime/gjmlc/core/Launcher.java
+++ src/com/mcgoodtime/gjmlc/core/Launcher.java
@@ -116,9 +116,40 @@ public class Launcher {
                 + jvmArgs + "-Djava.library.path=" + nativesPath + " " + "-classpath" + " " + libraries + "\"" + classPath + "\"" + " " + mainClass + " " + arg;
         System.out.println(cmd);
         try {
-            Runtime.getRuntime().exec(cmd);
+            Process process = Runtime.getRuntime().exec(cmd);
+            readProcessOutput(process);
+            process.waitFor();
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void readProcessOutput(Process process) {
+        read(process.getInputStream(), System.out);
+        read(process.getErrorStream(), System.err);
+    }
+
+    // 读取输入流
+    private static void read(InputStream inputStream, PrintStream out) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                out.println(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
